4da2ca69d9317e510883f9f279a23d6874c3c55c
key_gs <keygsgmod@gmail.com>
2022-03-17 01:57:39 -0700

    kjkhj

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 class IntroScreen extends React.Component {
     render() {
-        return (
-            <h1>Welcome</h1>
-        );
+        
     }
 }
+
+class Canvas extends React.Component {
+    constructor(props) {
+        super(props);
+        setInterval(() => {
+
+        }, 60);
+    }
+
+    createCanvas() {
+        var canvas = <canvas id="canvas"></canvas>     
+    }
+}
+
 
 ReactDOM.render(<IntroScreen />, document.getElementById("root"));
