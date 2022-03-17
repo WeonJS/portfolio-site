1cc9a45c1353aed27ca4c6800376b3cc5af260b8
key_gs <keygsgmod@gmail.com>
2022-03-16 20:22:19 -0700

    ye

---
+++ index.js
@@ -0,0 +1,9 @@
+class IntroScreen extends React.Component {
+    render() {
+        return (
+            <h1>Welcome</h1>
+        );
+    }
+}
+
+ReactDOM.render(<IntroScreen />, document.getElementsById("root"));
