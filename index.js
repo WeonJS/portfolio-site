f5e48433f755fb39624610187137e9a8a5968c05
Keon Davoudi <davoudikeon@gmail.com>
2022-03-18 12:28:48 -0700

    vas

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ class Canvas extends React.Component {
     componentDidMount() {
         const canvas = this.refs.canvas;
         const ctx = canvas.getContext('2d');
-        ctx.fillRect(0,0,50,50);
+        ctx.fillRect(0,0,canvas.width,canvas.height);
     }
 
     render() {
