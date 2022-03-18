327a740f37ce1a8a7fc6f5bfc431836647b4669b
Keon Davoudi <davoudikeon@gmail.com>
2022-03-18 12:17:13 -0700

    canvas fill

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ class Canvas extends React.Component {
     componentDidMount() {
         const canvas = this.refs.canvas;
         const ctx = canvas.getContext('2d');
+        ctx.fillRect(0,0,50,50);
     }
 
     render() {
