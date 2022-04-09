c1592cf58ddcf7bfb35d37a0382be468354670e7
key_gs <keygsgmod@gmail.com>
2022-04-09 15:30:05 -0700

    asdfa

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ class Canvas extends React.Component {
 
         const canvasStyling = {
             width: "100%",
-            height: "100vh",
+            height: "50vh",
             margin: "0px"
         };
 
